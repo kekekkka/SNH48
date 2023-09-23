da80d109d8fa07afecb78d11e1f8628240611a72
wkf <1337474068@qq.com>
2023-09-23 22:41:10 +0800

    更改

--- js/message.js
+++ js/message.js
@@ -1,34 +1,49 @@
-
-AV.init({
-    appId: "HJY0EWJWxivKGk6BKvcycCxB-gzGzoHsz",
-    appKey: "2VOup2awneLzPz9ejIRzBnZv",
-});
-
-var query = new AV.Query("Message");
-query.find()
-.then(
-    (messages) => {
-    let array = messages.map((item) => item.attributes)
-    array.forEach((item) => {
-        let li = document.createElement('li')
-        li.innerText = item.content
-        let messageList = document.querySelector('#messageList')      
-        messageList.appendChild(li)      
+function getData(){
+    $.ajax({
+        method: "GET",
+        url: "http://123.57.85.69:3001/comments",
     })
-})
+        .done(function (messages) {
+            let tempHtml = ''
+    
+            messages.resource.forEach((item) => {
+                console.log(item);
+                // let li = document.createElement('li')
+                // li.innerText = item.content
+                tempHtml += `<li>${item.name}:${item.content}</li>`    
+            })
+            console.log(tempHtml);
+            let messageList = document.querySelector('#messageList')
+            messageList.innerHTML = tempHtml
+        });
+    
+}
+getData()
 let myForm = document.querySelector('#postMessage')
 myForm.addEventListener('submit', function (e) {
-    e.preventDefault()
+    e.preventDefault()/*阻止默认事件*/
+    // e.stopPropagation()阻止事件冒泡
+    let name = myForm.querySelector('input[name=name]').value
     let content = myForm.querySelector('input[name=content]').value
-    var Message = AV.Object.extend("Message");
-    var message = new Message();
-    message.set({ content: content });
-    message.save().then((message) => {
-   console.log();
-        alert("成功")
-        window.location.reload()//刷新页面
+    $.ajax({
+        method: "POST",
+        url: "http://123.57.85.69:3001/comments",
+        contentType: "application/json",
+    //     data: { name: "测试", content:  myForm.querySelector('input[name=content]').value}
+    data: JSON.stringify({
+        name: name,
+        content: content,
+    }),
     })
-})
+        .done(function (messages) {
+            alert("成功")
+            getData()
+            myForm.querySelector('input[name=content]').value =''
+            myForm.querySelector('input[name=name]').value =''
+            })
+        });
+    
+
 
 
 
--- js/swiper.js
+++ js/swiper.js
@@ -1,10 +1,17 @@
-const swiper = new Swiper('.swiper', {              
-    loop: true,              
-    pagination: {
-        el: '.swiper-pagination',
-    },               
-    navigation: {
+var mySwiper = new Swiper('.swiper', {
+    autoplay: {
+        delay: 1000,//1秒切换一次
+        disableOnInteraction: false,
+        pauseOnMouseEnter: true,
+      },
+      loop: true,
+      navigation: {
         nextEl: '.swiper-button-next',
         prevEl: '.swiper-button-prev',
-    },
-});
+      },
+})
+
+//如果你初始化时没有定义Swiper实例，后面也可以通过Swiper的HTML元素来获取该实例
+new Swiper('.swiper')
+var mySwiper = document.querySelector('.swiper').swiper
+mySwiper.slideNext();
